b8abd0e27828649de8d5d8f11c88d55f89dcaab5
Wang Hualin <hnsdwhl@163.com>
2015-02-08 11:37:32 +0800

    Initial Commit

---
+++ GitDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitDemo
+//
+//  Created by Wang Hualin on 15-2-8.
+//  Copyright (c) 2015年 wanghualin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitDemo
+//
+//  Created by Wang Hualin on 15-2-8.
+//  Copyright (c) 2015年 wanghualin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
